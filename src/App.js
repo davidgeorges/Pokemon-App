98dce6e6b58d0f95519808e8228348eed2e5f5fc
jonathan <jonathanguerpillon@gmail.com>
2024-04-03 12:06:12 +0200

    finish router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './Pages/Home/Home';
 import Favorites from './Pages/Favorites/Favorites';
 
 function App() {
   return (
     <BrowserRouter>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/favorites" component={Favorites} />
-      </Switch>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/favorites" element={<Favorites />} />
+      </Routes>
     </BrowserRouter>
   );
 }
