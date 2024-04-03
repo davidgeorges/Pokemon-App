8a7edfba4337d65e80ec6d045639ed1330432fb2
David <georges.david.jk@gmail.com>
2024-04-03 12:00:48 +0200

    1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import './App.css';
-import PokemonDetail from './Components/PokemonDetail/PokemonDetail';
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Home from './Pages/Home/Home';
+import Favorites from './Pages/Favorites/Favorites';
 
 function App() {
   return (
-    <div>
-     <PokemonDetail></PokemonDetail>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/favorites" component={Favorites} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
--- src/Components/PokemonDetail/PokemonDetail.js
+++ src/Components/PokemonDetail/PokemonDetail.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './PokemonDetail.css';
 
 function PokemonDetail({ pokemonData }) {
 
