de5963555d1521bffa5319df84b2bab2710c9523
David <georges.david.jk@gmail.com>
2024-04-03 10:26:04 +0200

    First commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import PokemonDetail from './Components/PokemonDetail/PokemonDetail';
+
+function App() {
+  return (
+    <div>
+     <PokemonDetail></PokemonDetail>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/PokemonDetail/PokemonDetail.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+function PokemonDetail({ pokemonData }) {
+
+  return (
+    <li class="animating" >
+        <img src={"https://assets.pokemon.com/assets/cms2/img/pokedex/detail/002.png"} alt="profile"/>
+
+      <div class="pokemon-info">
+        <p class="id">
+          <span class="number-prefix">Nº&nbsp;</span>0002
+        </p>
+        <h5>Herbizarre</h5>
+
+        <div class="abilities">
+          <span class="pill background-color-grass">Plante</span>
+        </div>
+        <div class="abilities">
+          <span class="pill background-color-poison">Poison</span>
+        </div>
+      </div>
+    </li>
+  );
+}
+
+export default PokemonDetail;
