810dda5e03dd3819ad348d03c29766d0b546316b
David <georges.david.jk@gmail.com>
2024-04-03 17:29:00 +0200

    test battle

--- src/Pages/Test/Test.js
+++ src/Pages/Test/Test.js
@@ -1,10 +1,13 @@
 import React from 'react';
-
+import './Test.css'
 function Test() {
 
   return (
-    <div >
-        <img src={"./src/images/bg.png"} alt='p'/>
+    <div class="container" style={{ backgroundImage: `url(${require("./bg.png")})` }}> 
+        <img class="pokemon-1" src={"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/showdown/back/1.gif"} alt='pokemon-1'/>
+        <img class="pokemon-2" src={"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/showdown/4.gif"} alt='pokemon-2'/>
+        <img class="pokemon-1-hp" src={require('./hp-p1-f.png')} alt='pokemon-1-hp'/>
+        <img class="pokemon-2-hp" src={require('./hp-p2-f.png')} alt='pokemon-2-hp'/>
     </div>
   );
 }
